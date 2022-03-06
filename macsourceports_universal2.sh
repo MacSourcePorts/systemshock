fbcb5103d20e8896653f14b6fdff1a84ba4893ac
Tom Kidd <tomkidd@gmail.com>
2022-03-06 17:45:34 -0600

    Now builds and links fluidsynths

--- macsourceports_universal2.sh
+++ macsourceports_universal2.sh
@@ -13,7 +13,17 @@ source ../MSPScripts/constants.sh
 
 rm -rf ${BUILT_PRODUCTS_DIR}
 
-# create makefiles with cmake
+# build x86_64 fluidsynth
+cd build_ext/fluidsynth-lite
+rm -rf lib/x86_64
+mkdir lib/x86_64
+make clean
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-arch x86_64" .
+cmake --build .
+cp -a src/libfluidsynth* lib/x86_64
+cd ../..
+
+# create x86_64 makefiles with cmake
 rm -rf ${X86_64_BUILD_FOLDER}
 mkdir ${X86_64_BUILD_FOLDER}
 cd ${X86_64_BUILD_FOLDER}
@@ -23,11 +33,29 @@ cmake -DCMAKE_C_FLAGS="-arch x86_64" \
     -DENABLE_SOUND=ON \
     -DSDL2_DIR=/usr/local/opt/sdl2/lib/cmake/SDL2 \
     -DSDL2_MIXER_LIBRARIES=/usr/local/lib/libSDL2_mixer.dylib \
-    -DENABLE_FLUIDSYNTH="DDD" \
-    -DFLUIDSYNTH_LIBRARY=./build_ext/fluidsynth-lite/lib/x86_64/libfluidsynth.dylib \
+    -DENABLE_FLUIDSYNTH="BUNDLED" \
     ..
 
+# perform x86_64 build with make
+cd ${X86_64_BUILD_FOLDER}
+make -j$NCPU
+
+#tweak x86_64 install name
 cd ..
+echo install_name_tool -change $PWD/build_ext/fluidsynth-lite/src/libfluidsynth.1.dylib $PWD/build_ext/fluidsynth-lite/lib/x86_64/libfluidsynth.1.dylib ${X86_64_BUILD_FOLDER}/systemshock
+install_name_tool -change $PWD/build_ext/fluidsynth-lite/src/libfluidsynth.1.dylib $PWD/build_ext/fluidsynth-lite/lib/x86_64/libfluidsynth.1.dylib ${X86_64_BUILD_FOLDER}/systemshock
+
+# build arm64 fluidsynth
+cd build_ext/fluidsynth-lite
+rm -rf lib/arm64
+mkdir lib/arm64
+make clean
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-arch arm64" .
+cmake --build .
+cp -a src/libfluidsynth* lib/arm64
+cd ../..
+
+# create arm64 makefiles with cmake
 rm -rf ${ARM64_BUILD_FOLDER}
 mkdir ${ARM64_BUILD_FOLDER}
 cd ${ARM64_BUILD_FOLDER}
@@ -37,20 +65,17 @@ cmake -DCMAKE_C_FLAGS="-arch arm64 -DSDL_DISABLE_IMMINTRIN_H" \
     -DENABLE_SOUND=ON \
     -DSDL2_DIR=/opt/homebrew/opt/sdl2/lib/cmake/SDL2 \
     -DSDL2_MIXER_LIBRARIES=/opt/homebrew/lib/libSDL2_mixer.dylib \
-    -DENABLE_FLUIDSYNTH="DDD" \
-    -DFLUIDSYNTH_LIBRARY=./build_ext/fluidsynth-lite/lib/arm64/libfluidsynth.dylib  \
+    -DENABLE_FLUIDSYNTH="BUNDLED" \
     ..
 
-perform builds with make
-cd ..
-cd ${X86_64_BUILD_FOLDER}
-make -j$NCPU
-
-cd ..
+# perform arm64 build with make
 cd ${ARM64_BUILD_FOLDER}
 make -j$NCPU
 
-# cd ..
+#tweak arm64 install name
+cd ..
+echo install_name_tool -change $PWD/build_ext/fluidsynth-lite/src/libfluidsynth.1.dylib $PWD/build_ext/fluidsynth-lite/lib/arm64/libfluidsynth.1.dylib ${ARM64_BUILD_FOLDER}/systemshock
+install_name_tool -change $PWD/build_ext/fluidsynth-lite/src/libfluidsynth.1.dylib $PWD/build_ext/fluidsynth-lite/lib/arm64/libfluidsynth.1.dylib ${ARM64_BUILD_FOLDER}/systemshock
 
 # create the app bundle
 # "../MSPScripts/build_app_bundle.sh"
